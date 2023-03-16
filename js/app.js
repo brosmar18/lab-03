16a40bcf3b2ac0c13b70fe4847df3b9e2cdcc3f6
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-03-15 21:12:28 -0500

    total points added and shown to user

--- js/app.js
+++ js/app.js
@@ -1,19 +1,20 @@
 alert('Welcome to my guessing game!');
 
-
+let userPoints = 0;
 
 let attempts = 0;
 while(attempts <= 4) {
   attempts++;
 
-  let number_answer = prompt('Guess a number between 1 and 10');
+  let numberAnswer = prompt('Guess a number between 1 and 10');
 
-  if(number_answer < 6) {
+  if(numberAnswer < 6) {
     alert('Your guess is too low!');
-  } else if(number_answer > 6){
+  } else if(numberAnswer > 6){
     alert('Your answer is too high!');
   } else {
     alert('You are correct!');
+    userPoints++;
     break;
   }
 }
@@ -23,14 +24,17 @@ alert('The answer is 6');
 let attempts2 = 0;
 while(attempts2 <=6) {
   attempts2++;
-  let states_visited = ['illinois', 'nebraska', 'florida', 'indiana', 'wisconsin'];
+  let statesVisited = ['illinois', 'nebraska', 'florida', 'indiana', 'wisconsin'];
   let guessAnswer = prompt('Guess one of my favorite states to visit').toLowerCase().trim();
 
-  if (states_visited.includes(guessAnswer)) {
+  if (statesVisited.includes(guessAnswer)) {
     alert('You are correct! ' + guessAnswer + ' is one of my favorite states to visit.');
+    userPoints++;
     break;
   } else {
     alert('Wrong!');
   }
 }
 alert('My favorite states to visit are: Illinois, Nebraska, Florida, Indiana, and Wisconsin');
+
+alert('Your score is: ' + userPoints +'/2');
