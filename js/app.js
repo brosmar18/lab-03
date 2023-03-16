3f68e2c078788cf5cd6124733d3dbee1cea2420c
Bryan Garduno Gonzalez <brosmarcodetech@icloud.com>
2023-03-16 10:00:28 -0500

    random number guess added

--- js/app.js
+++ js/app.js
@@ -2,23 +2,53 @@ alert('Welcome to my guessing game!');
 
 let userPoints = 0;
 
-let attempts = 0;
-while(attempts < 4) {
-  attempts++;
+// let attempts = 0;
+// while(attempts < 4) {
+//   attempts++;
 
-  let numberAnswer = prompt('Guess a number between 1 and 10');
+//   let numberAnswer = prompt('Guess a number between 1 and 10');
 
-  if(numberAnswer < 6) {
-    alert('Your guess is too low!');
-  } else if(numberAnswer > 6){
-    alert('Your answer is too high!');
-  } else {
-    alert('You are correct!');
-    userPoints++;
+//   if(numberAnswer < 6) {
+//     alert('Your guess is too low!');
+//   } else if(numberAnswer > 6){
+//     alert('Your answer is too high!');
+//   } else {
+//     alert('You are correct!');
+//     userPoints++;
+//     break;
+//   }
+// }
+// alert('The answer is 6');
+
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+let attempts = 4;
+let question = 'Guess an integer from 1 to 10';
+
+while(attempts > 0 ) {
+  let guessNumber = parseInt(prompt(question + 'You have ' + attempts + ' try\'s to go.'));
+  // What happens if they don't want to play. Handle the cancel.
+  if(!guessNumber) {
     break;
   }
+  if(guessNumber === randomNumber) {
+    alert('You guessed the correct number! Good job.');
+    userPoints++;
+    attempts = 0;
+  } else {
+    question = 'You wer close. Try again';
+    if(guessNumber < randomNumber) {
+      alert('Yoo low! Try again');
+    }
+    if(guessNumber > randomNumber) {
+      alert('Too high! Try again');
+    }
+    attempts--;
+  }
 }
-alert('The answer is 6');
+
+
+
+alert('The correct number was ' + randomNumber);
 
 
 let attempts2 = 0;
